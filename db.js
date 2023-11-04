8e87412bd97de4ff97a350e6111fb3246d99c802
KRVitalik <karmelitavitalik@gmail.com>
2023-11-04 20:04:47 +0200

    Update db.js

--- db.js
+++ db.js
@@ -5,13 +5,7 @@ import pkg from 'pg';
 const {Pool} = pkg;
 
 const pool = new Pool({
-  type: "postgres",
-  host: process.env.HOSTNAME,
-  port: 5432,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE_NAME,
-  synchronize: true,
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 export default pool;
